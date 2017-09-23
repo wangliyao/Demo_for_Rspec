295b6a95a6b17dcb93acaa68eab4addf8abd17af
wangliyao <755210570@qq.com>
2017-09-23 22:39:51 +0800

    添加session_controller测试，添加user_controller测试

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
+  describe 'Post #create' do
+    it 'success create ' do
+      post :create, user: attributes_for(:user, code: nil)
+      expect(response).to be_success
+    end
+  end
 
+  describe 'Post #,msg_login' do
+    it 'success msg_login ' do
+      post :msg_login, user: attributes_for(:user, password: nil)
+      expect(response).to be_success
+    end
+  end
+
+  describe 'Post #,login_out' do
+    it 'success login_out ' do
+      get :login_out
+      expect(response).to redirect_to(:root)
+    end
+  end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,11 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
-    describe 'create' do
-      it "测试保存用户成功" do
-       params = { name: '30001'}
-        post :create, params
-        expect(response).to redirect_to(:new_password_users)
-     end
+  describe 'create' do
+    it "测试保存用户成功" do
+      post :create, user: FactoryGirl.attributes_for(:user, password: nil)
+      expect(response).to redirect_to(:new_password_users)
     end
+  end
+
+  describe 'password save' do
+    it "测试用户保存密码成功" do
+      post :create_password, user: FactoryGirl.attributes_for(:user)
+      expect(response).to be_success
+    end
+  end
+
+  describe 'change password redirect root' do
+    it "测试用户修改密码成功" do
+      user = create(:user)
+      get :change, id: user
+      expect(response).to be_success
+    end
+  end
 end
